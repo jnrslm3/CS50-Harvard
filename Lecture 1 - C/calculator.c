4173492755939568f1f949b655ba45531407d5f3
jnrslm3 <jumabekovn43@gmail.com>
2025-10-13 12:44:19 +0600

    some changes in calculator

--- Lecture 1 - C/calculator.c
+++ Lecture 1 - C/calculator.c
@@ -1,17 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int add(int a, int b);
+// Second program
 
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%i\n", add(x, y));
+    double z = (double) x / (double) y;
+    printf("%.20f\n", z);
 }
 
-int add(int a, int b)
-{
-    return a + b;
-}
+//First program
+
+
+// int add(int a, int b);
+
+// int main(void)
+// {
+//     int x = get_int("x: ");
+//     int y = get_int("y: ");
+
+//     printf("%i\n", add(x, y));
+// }
+
+// int add(int a, int b)
+// {
+//     return a + b;
+// }
